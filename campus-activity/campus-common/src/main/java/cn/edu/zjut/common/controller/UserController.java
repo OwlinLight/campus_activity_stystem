7317e2fb88ec357915e9292fbab5abe359b52845
徐伟峰 <2207394788@qq.com>
2021-01-05 19:07:23 +0800

    用户的登录和注册的密码加密

--- campus-activity/campus-common/src/main/java/cn/edu/zjut/common/controller/UserController.java
+++ campus-activity/campus-common/src/main/java/cn/edu/zjut/common/controller/UserController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import cn.edu.zjut.common.domain.Sha256;
 
 
 @Api(
@@ -27,6 +28,8 @@ public class UserController {
     @ResponseBody
     public CommonResult<User> loginUser(@RequestParam(value = "userName") Long staff_id, @RequestParam(value = "password") String password) {
         CommonResult commonResult;
+        //把password通过加密后再进行登录
+        password = Sha256.getSHA256StrJava(password);
         User user = (User) userserver.loginUser(staff_id, password);
         if (user == null) {
             commonResult = CommonResult.failed("登录失败");
@@ -41,6 +44,10 @@ public class UserController {
     @ResponseBody
     public CommonResult createUser(@RequestBody User user) {
         CommonResult commonResult;
+        //把user的password通过加密后进行注册
+        String Pass = user.getPassword();
+        Pass = Sha256.getSHA256StrJava(Pass);
+        user.setPassword(Pass);
         int count = userserver.createUser(user);
         if (count == 1) {
             commonResult = CommonResult.success(user);
---
+++ campus-activity/campus-common/src/main/java/cn/edu/zjut/common/domain/Sha256.java
@@ -0,0 +1,58 @@
+package cn.edu.zjut.common.domain;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class Sha256 {
+
+    public static void main(String [] argv){
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        System.out.print("str = " + str + "\n");
+        System.out.print("sha = " + getSHA256StrJava(str));
+    }
+    /**
+     * 利用java原生的摘要实现SHA256加密
+     *
+     * @param str 加密后的报文
+     * @return
+     */
+    public static String getSHA256StrJava(String str) {
+        MessageDigest messageDigest;
+        String encodeStr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(str.getBytes("UTF-8"));
+            encodeStr = byte2Hex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encodeStr;
+    }
+
+    /**
+     * 将byte转为16进制
+     *
+     * @param bytes
+     * @return
+     */
+    private static String byte2Hex(byte[] bytes) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        for (int i = 0; i < bytes.length; i++) {
+            temp = Integer.toHexString(bytes[i] & 0xFF);
+            if (temp.length() == 1) {
+                //1得到一位的进行补0操作
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(temp);
+        }
+        return stringBuffer.toString();
+    }
+
+
+}
